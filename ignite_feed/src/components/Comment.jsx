7b1d5b556feff26cfc301b18bdd23cf3b1f05ad7
Marinalm15 <marinalm15@hotmail.com>
2023-10-21 12:57:15 -0300

    adiciona seção de comentários

---
+++ ignite_feed/src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import { Trash } from '@phosphor-icons/react';
+import styles from './Comment.module.css';
+import { ThumbsUp } from '@phosphor-icons/react';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://th.bing.com/th/id/OIP.tvaMwK3QuFxhTYg4PSNNVAHaHa?pid=ImgDet&rs=1"/>
+        <div className={styles.commentBox}>
+            <div className={styles.commentContent}>
+                <header>
+                    <div className={styles.authorAndTime}>
+                        <strong>Diego Fernandes</strong>
+                        <time title="02 de Outubro às 20:16h" dateTime="2023-09-02 20:16:45">Cerca de 1h atrás</time>
+                    </div>
+                    <button title="Deletar comentário">
+                        <Trash size={24}/>
+                    </button>
+                </header>
+
+                <p>Muito bom, parabéns!</p>
+            </div>
+            <footer>
+                <button>
+                    <ThumbsUp/>
+                    Aplaudir <span>20</span>
+                </button>
+            </footer>
+        </div>
+        </div>
+    )
+}
