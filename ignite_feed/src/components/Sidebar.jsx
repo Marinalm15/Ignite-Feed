6b5cd68549c16b144556666ec48f2d200c7e9e3b
Marinalm15 <marinalm15@hotmail.com>
2023-10-21 12:55:42 -0300

    edita sidebar

--- ignite_feed/src/components/Sidebar.jsx
+++ ignite_feed/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from "./Sidebar.module.css";
+import { PencilLine } from "@phosphor-icons/react";
 
 export function Sidebar() {
     return (
@@ -7,12 +8,14 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1508808703020-ef18109db02f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fGZsb3dlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=300&q=40"/>
 
         <div className={styles.profile}>
+          <img className={styles.avatar} src="../assets/foto.jpg"/>
           <strong>Marina Mendonça</strong>
           <span>Web Developer</span>
         </div>
 
         <footer>
           <a href="#">
+          <PencilLine size={20} />
             Editar o seu perfil
           </a>
         </footer>
