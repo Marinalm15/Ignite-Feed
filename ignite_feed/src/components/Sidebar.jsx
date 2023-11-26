5a4f9a04947c14368f944590f7445442348112ff
Marinalm15 <marinalm15@hotmail.com>
2023-11-26 12:35:39 -0300

    fix warning de key e altera foto da sidebar

--- ignite_feed/src/components/Sidebar.jsx
+++ ignite_feed/src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ export function Sidebar() {
         src="https://images.unsplash.com/photo-1508808703020-ef18109db02f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fGZsb3dlcnN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=300&q=40"/>
 
         <div className={styles.profile}>
-          <img className={styles.avatar} src="../assets/foto.jpg"/>
+          <img className={styles.avatar} src="https://th.bing.com/th/id/OIP.PqBYGErQeWQWhbA_VeUBDQHaHa?w=189&h=189&c=7&r=0&o=5&dpr=1.3&pid=1.7"/>
           <strong>Marina Mendonça</strong>
           <span>Web Developer</span>
         </div>
