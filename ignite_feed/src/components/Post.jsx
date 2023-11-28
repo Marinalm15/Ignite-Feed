f21e01efcab3fa28f4c339e835b492ec00689ae7
Marinalm15 <marinalm15@hotmail.com>
2023-11-28 19:31:33 -0300

    validação do formulário de comentário

--- ignite_feed/src/components/Post.jsx
+++ ignite_feed/src/components/Post.jsx
@@ -6,7 +6,7 @@ import styles from "./Post.module.css";
 export function Post({author, publishedAt, content}) {
     const [comments, setComments] = useState([
         "Post muito bacana, parabéns!"
-])
+    ])
 
     const [newCommentText, setNewCommentText] = useState("")
 
@@ -25,7 +25,12 @@ export function Post({author, publishedAt, content}) {
     }
 
     function handleNewCommentChange() {
+        event.target.setCustomValidity("")
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity("Este campo é obrigatório!")
     }
 
     function deleteComment(commentToDelete) {
@@ -34,6 +39,8 @@ export function Post({author, publishedAt, content}) {
         })
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length === 0
 
     return (
         <article className={styles.post}>
@@ -67,10 +74,12 @@ export function Post({author, publishedAt, content}) {
                 placeholder="Deixe um comentário"
                 value={newCommentText}
                 onChange={handleNewCommentChange}
+                onInvalid={handleNewCommentInvalid}
+                required={true}
                 />
 
                 <footer>
-                <button type="submit">Publicar</button>
+                <button type="submit" disabled={isNewCommentEmpty}>Publicar</button>
                 </footer>
             </form>
 
